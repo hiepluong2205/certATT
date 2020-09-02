d5a931ad19e398a1f0cb09f17099131e6086c5ba
Hiep Luong <>
2020-09-01 17:10:43 -0700

    test docs/index.html

---
+++ src/main/java/com/hluong/certATT/PageHome.java
@@ -0,0 +1,2 @@
+public class PageHome extends LoadableComponent {
+}
---
+++ src/test/java/com/hluong/certATT/Test001.java
@@ -0,0 +1,10 @@
+package com.hluong.certATT;
+
+import org.testng.annotations.Test;
+
+public class Test001 {
+    @Test
+    public void test() {
+//        PageHome.load();
+    }
+}
